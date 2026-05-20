5b23cca2985e3d42dd5fe35150fd6ffc3c794443
vighnesh chiluka <vighnesh.chiluka25@gmail.com>
2026-05-20 22:43:18 +0530

    Updated latest TextUtils UI and routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
   return (
     <nav className= {`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="./">
+        <Link className="navbar-brand" to="/">
           {props.title}
         </Link>
         <button
@@ -28,7 +28,7 @@ export default function Navbar(props) {
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="../about">
+              <Link className="nav-link" to="/about">
                 {props.aboutus}
               </Link>
             </li>
