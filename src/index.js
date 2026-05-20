7e64edf6fa3eeb8863ce9e85e3066d3ffece9fcb
vighnesh chiluka <vighnesh.chiluka25@gmail.com>
2026-05-20 22:36:25 +0530

    Updated latest TextUtils UI and routing

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
